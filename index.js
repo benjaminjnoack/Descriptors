af9d3ac5f8b27dc41d64266422137a458b62e3f3
Ben Noack <bnoack@powerley.com>
2017-02-16 22:10:28 -0500

    product info

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 /**
  * Created by ben on 2/16/17.
+ *
+ * { '$': { id: '0214', name: 'Kaipule' },
+          Product:
+           [ { '$':
+                { type: '0002',
+                  id: '0001',
+                  name: 'IM20 Door/Window Sensor',
+                  config: 'kaipule/im20.xml' } } ] },
+
  */
 'use strict';
 
@@ -7,6 +16,15 @@ const
   fs      = require('fs'),
   util    = require('util'),
   xml2js  = require('xml2js');
+
+const
+  META                        = '$',
+  ID                          = 'id',
+  MANUFACTURER_SPECIFIC_DATA  = 'ManufacturerSpecificData',
+  MANUFACTURER                = 'Manufacturer',
+  NAME                        = 'name',
+  PRODUCT                     = 'Product',
+  TYPE                        = 'type';
 
 let parseString = xml2js.parseString;
 let path = `${__dirname}/config/manufacturer_specific.xml`;
@@ -18,15 +36,28 @@ parseString(file, (err, result) => {
 });
 
 function readResults(results) {
-  results = results['ManufacturerSpecificData'];
-  results = results['Manufacturer'];//An Array
+  results = results[MANUFACTURER_SPECIFIC_DATA];
+  results = results[MANUFACTURER];//An Array
 
-  let meta, name, id;
+  let meta, name, manufacturerId, products, productId, productType, productName;
 
   results.forEach((manufacturer) => {
-    meta = manufacturer['$'];
-    id = meta['id'];
-    name = meta['name'];
-    console.log(`${id} ${name}`);
+    meta = manufacturer[META];
+    manufacturerId = meta[ID];
+    name = meta[NAME];
+    products = manufacturer[PRODUCT];
+
+    console.log(`Manufacturer: ${manufacturerId} ${name}`);
+
+    if (products) {
+      products.forEach(product => {
+        product = product[META];
+        productId = product[ID];
+        productType = product[TYPE];
+        productName = product[NAME];
+
+        console.log(`\tProduct: ${productName} ${productType} ${productId}`);
+      });
+    }
   });
 }
