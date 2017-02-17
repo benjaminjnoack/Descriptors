62ae2c31b5418afcc0efc3b0f4bfe0a1cc63dd99
Ben Noack <bnoack@powerley.com>
2017-02-16 22:28:04 -0500

    completed

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const
 let parseString = xml2js.parseString;
 let path = `${__dirname}/config/manufacturer_specific.xml`;
 
-
 let file = fs.readFileSync(path);
 parseString(file, (err, result) => {
   readResults(result);
@@ -39,25 +38,52 @@ function readResults(results) {
   results = results[MANUFACTURER_SPECIFIC_DATA];
   results = results[MANUFACTURER];//An Array
 
-  let meta, name, manufacturerId, products, productId, productType, productName;
+  let meta, file, path,
+    manufacturerName, manufacturerId,
+    products, productId, productType, productName;
 
   results.forEach((manufacturer) => {
     meta = manufacturer[META];
-    manufacturerId = meta[ID];
-    name = meta[NAME];
+    manufacturerId = parseInt(meta[ID], 16);
+    manufacturerName = meta[NAME];
     products = manufacturer[PRODUCT];
 
-    console.log(`Manufacturer: ${manufacturerId} ${name}`);
+    console.log(`Manufacturer: ${manufacturerId} ${manufacturerName}`);
 
     if (products) {
       products.forEach(product => {
         product = product[META];
-        productId = product[ID];
-        productType = product[TYPE];
+        productId = parseInt(product[ID], 16);
+        productType = parseInt(product[TYPE], 16);
         productName = product[NAME];
 
         console.log(`\tProduct: ${productName} ${productType} ${productId}`);
+
+        path = categoryProductId(manufacturerId, productType, productId);
+        path = `${__dirname}/descriptors/${path}.json`;
+        file = generateTemplate(manufacturerName, productName);
+        fs.writeFileSync(path, file);
       });
     }
   });
+}
+
+function categoryProductId(manufacturerId, productTypeId, productId) {
+  const id = Buffer.allocUnsafe(6);
+  id.writeUInt16BE(manufacturerId);
+  id.writeUInt16BE(productTypeId, 2);
+  id.writeUInt16BE(productId, 4);
+  return id.toString('hex');
+}
+
+function generateTemplate(manufacturer, product) {
+  let template = {
+    commands: {},
+    configurations: [],
+    manufacturer: manufacturer,
+    meta: {},
+    product: product,
+  };
+
+  return JSON.stringify(template, null, 4);
 }
