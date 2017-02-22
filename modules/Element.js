4cc589da5b41b4ceaa318b7e5e9d00ab1d1f13b4
Ben Noack <bnoack@powerley.com>
2017-02-22 09:43:56 -0500

    tidy Element

--- modules/Element.js
+++ modules/Element.js
@@ -2,7 +2,7 @@
  * Created by ben on 2/21/17.
  */
 'use strict';
-//TODO send meta to super, set meta is always called, becomes print
+
 class Element {
   constructor(meta) {
     if (new.target === Element)
@@ -10,11 +10,11 @@ class Element {
 
     this.meta = meta;
   }
-  //return this._meta created from this
+
   get meta() {
     return this;//TODO
   }
-  //TODO set this._meta to an enumeration of properties from the meta arg
+
   set meta(meta) {
     Object.assign(this, meta);
   }
